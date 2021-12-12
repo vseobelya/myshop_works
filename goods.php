46e1681d74a374d6cd5895992708e80aad76e999
vseobelya <82372176+vseobelya@users.noreply.github.com>
2021-12-12 14:41:38 +0300

    php file to get list of goods

---
+++ goods.php
@@ -0,0 +1,42 @@
+<?php header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods:POST,GET,PUT,DELETE');
+header('Access-Control-Allow-Headers: content-type or other');
+header('Content-Type: application/json');
+
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "myshop";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//Add user
+// if(isset($_POST['myEmail']))
+// {
+//     $sql = "INSERT INTO userdetails (email, username)
+//         VALUES ('".$_POST['myEmail']."', '".$_POST['myUsername']."')";
+//     if (mysqli_query($conn,$sql)) {
+//     $data = array("data" => "You Data added successfully");
+//         echo json_encode($data);
+//     } else {
+//         $data = array("data" => "Error: " . $sql . "<br>" . $conn->error);
+//         echo json_encode($data);
+//
+//     }
+// }
+// else
+// {
+    //get all users details
+    $trp = mysqli_query($conn, "SELECT * from goods");
+    $rows = array();
+    while($r = mysqli_fetch_assoc($trp)) {
+        $rows[] = $r;
+    }
+
+    print json_encode($rows);
+// }
+die();
